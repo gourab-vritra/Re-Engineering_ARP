26aaa0065d542d9c1b0cb4e1e143d983b2400829
gourab-vritra <gourab.vritra@gmail.com>
2017-11-12 21:28:02 +0530

    File Upload

---
+++ Web/app/controllers/homeController.js
@@ -0,0 +1,23 @@
+
+/*
+angular.module("app").controller('HomeController',['$scope','$http',function($scope,$http) {
+    //we will have some method which will fetch the latest articles from the server and assign those to a property of scope.
+    
+    /*this.getMessage = $http.get("http://localhost:12121/api/Blog/index").then(function(res) {
+        return res.data.message;
+    })
+    
+    this.getLatestArticles = function(){
+        return [{
+                id: 1,
+                title: 'Freedom of Expression/Right to Speech',
+                singleLine: 'WE, THE PEOPLE OF INDIA, having solemnly resolved to constitute India into a SOVEREIGN SOCIALIST SECULAR DEMOCRATIC REPUBLIC and to secure to all its citizens: JUSTICE, LIBERTY, EQUALITY and FRATERNITY.',
+                src: 'Images/Blog1_Header.png'
+            }
+        ];
+    };
+    
+    
+    $scope.articlesArr = this.getLatestArticles();
+}]);
+*/
